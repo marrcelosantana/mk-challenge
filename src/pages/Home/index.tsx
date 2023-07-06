aeb43a4d5190ba2b2af02c3a6a8ef7a5ba77ba4e
marrcelosantana <marcelosantana@alu.ufc.br>
2023-07-06 13:52:10 -0300

    [MK-Challenge] - bug fix

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -24,14 +24,6 @@ import {
 
 export function Home() {
   const { t } = useTranslation();
-  const options = [
-    "Produto",
-    "Cliente",
-    "Anexar Arquivo",
-    "Pagamentos",
-    "Contrato",
-  ];
-
   const [optionSelected, setOptionSelected] = useState("Cliente");
 
   return (
@@ -48,19 +40,48 @@ export function Home() {
           </Titles>
 
           <Menu>
-            {options.map((option, index) => (
-              <Option key={index + 1} onClick={() => setOptionSelected(option)}>
-                <OptionNumber>{index + 1}</OptionNumber>
-                <OptionName option={option} optionSelected={optionSelected}>
-                  {option}
-                </OptionName>
-              </Option>
-            ))}
+            <Option onClick={() => setOptionSelected("Produto")}>
+              <OptionNumber>1</OptionNumber>
+              <OptionName option={"Produto"} optionSelected={optionSelected}>
+                {t("Produto")}
+              </OptionName>
+            </Option>
+
+            <Option onClick={() => setOptionSelected("Cliente")}>
+              <OptionNumber>2</OptionNumber>
+              <OptionName option={"Cliente"} optionSelected={optionSelected}>
+                {t("Cliente")}
+              </OptionName>
+            </Option>
+
+            <Option onClick={() => setOptionSelected("Anexar arquivo")}>
+              <OptionNumber>3</OptionNumber>
+              <OptionName
+                option={"Anexar arquivo"}
+                optionSelected={optionSelected}
+              >
+                {t("Anexar arquivo")}
+              </OptionName>
+            </Option>
+
+            <Option onClick={() => setOptionSelected("Pagamentos")}>
+              <OptionNumber>4</OptionNumber>
+              <OptionName option={"Pagamentos"} optionSelected={optionSelected}>
+                {t("Pagamentos")}
+              </OptionName>
+            </Option>
+
+            <Option onClick={() => setOptionSelected("Contrato")}>
+              <OptionNumber>5</OptionNumber>
+              <OptionName option={"Contrato"} optionSelected={optionSelected}>
+                {t("Contrato")}
+              </OptionName>
+            </Option>
           </Menu>
           <Subtotal />
 
           {optionSelected === "Cliente" && <Client></Client>}
-          {optionSelected === "Anexar Arquivo" && <File></File>}
+          {optionSelected === "Anexar arquivo" && <File></File>}
         </FormContainer>
       </Content>
     </Container>
