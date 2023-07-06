2fd7cb1f8a8dd0818b4b582bc8af6af3683fd86c
marrcelosantana <marcelosantana@alu.ufc.br>
2023-07-05 22:24:03 -0300

    [MK-Challenge] - Update menu

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,19 +1,38 @@
+import { useState } from "react";
+
 import { Header } from "@/components/Header";
 import { Subtotal } from "@/components/Subtotal";
-import { Menu } from "@/components/Menu";
+
 import { Client } from "@/components/Forms/Client";
+import { File } from "@/components/Forms/File";
 
 import {
   Column,
   Container,
   Content,
   FormContainer,
+  Menu,
+  Option,
+  OptionName,
+  OptionNumber,
   Subtitle,
   Title,
   Titles,
 } from "./styles";
 
 export function Home() {
+  const tabs = [
+    "Produto",
+    "Cliente",
+    "Anexar Arquivo",
+    "Pagamentos",
+    "Contrato",
+  ];
+
+  const status = ["InProgress", "Done"];
+
+  const [tabSelected, setTabSelected] = useState("Cliente");
+
   return (
     <Container>
       <Header />
@@ -27,10 +46,18 @@ export function Home() {
             <Subtitle>Nova venda</Subtitle>
           </Titles>
 
-          <Menu />
+          <Menu>
+            {tabs.map((tab, index) => (
+              <Option key={index + 1} onClick={() => setTabSelected(tab)}>
+                <OptionNumber>{index + 1}</OptionNumber>
+                <OptionName>{tab}</OptionName>
+              </Option>
+            ))}
+          </Menu>
           <Subtotal />
 
-          <Client />
+          {tabSelected === "Cliente" && <Client></Client>}
+          {tabSelected === "Anexar Arquivo" && <File></File>}
         </FormContainer>
       </Content>
     </Container>
--- src/pages/Home/styles.ts
+++ src/pages/Home/styles.ts
@@ -61,3 +61,64 @@ export const Subtitle = styled.h1`
   font-weight: 700;
   color: ${({ theme }) => theme.COLORS.GREEN};
 `;
+
+export const Menu = styled.div`
+  width: 100%;
+  height: 3.5625rem;
+  border-radius: 20px;
+  background-color: ${({ theme }) => theme.COLORS.WHITE};
+  margin-bottom: 32px;
+  display: flex;
+  align-items: center;
+  justify-content: space-around;
+
+  @media (max-width: 414px) {
+    height: 3.125rem;
+  }
+`;
+
+export const Option = styled.div`
+  width: 20%;
+  height: 57px;
+  border-radius: 0 20px 20px 0;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  gap: 10px;
+  cursor: pointer;
+
+  @media (max-width: 414px) {
+    gap: 5px;
+  }
+
+  background: none;
+  border: 0;
+
+  &:hover {
+    opacity: 1;
+  }
+`;
+
+export const OptionNumber = styled.span`
+  font-size: 14px;
+  font-weight: 700;
+  color: ${({ theme }) => theme.COLORS.GREEN};
+
+  @media (max-width: 414px) {
+    font-size: 9px;
+  }
+`;
+
+export const OptionName = styled.p`
+  font-size: 14px;
+  font-weight: 700;
+  color: ${({ theme }) => theme.COLORS.DARK_GREEN};
+
+  &:hover {
+    text-decoration: underline;
+  }
+
+  @media (max-width: 414px) {
+    font-size: 9px;
+  }
+`;
